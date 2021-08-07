a920a9e0f3053b868303c787bd5f46392507b7b0
Marie Serykh <marie.serykh@gmail.com>
2021-08-07 13:04:59 +0200

    feat: add the solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessNumber = Math.ceil((this.max + this.min) / 2);
+        return this.guessNumber;
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
